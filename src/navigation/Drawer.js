16316a96d2b059f09a6eaa04552d97afb0f06526
AmuchasteguiMarcos317 <amuchasteguimarcos317@gmail.com>
2022-09-29 11:39:52 -0300

    Sign In screen

--- src/navigation/Drawer.js
+++ src/navigation/Drawer.js
@@ -4,6 +4,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from '../../src/screens/Home'
 import Cities from '../screens/Cities';
 import Details from '../screens/Details';
+import SignIn from '../screens/SignIn';
 import UnderConstruction from '../screens/UnderConstruction';
 import Stack from './Stack';
 
@@ -15,6 +16,7 @@ export default function Drawer() {
         <DrawerNavigation.Navigator initialRouteName='Home'  screenOptions={{drawerStyle:{ backgroundColor:'#d7e7e3'}}}>
             <DrawerNavigation.Screen name="MyTinerary" component={Stack} />
             <DrawerNavigation.Screen name="Cities" component={Cities} />
+            <DrawerNavigation.Screen name="SignIn" component={SignIn} />
         </DrawerNavigation.Navigator>
     );
 }
--- src/navigation/Stack.js
+++ src/navigation/Stack.js
@@ -3,7 +3,7 @@ import Home from '../../src/screens/Home'
 import Cities from '../screens/Cities';
 import Details from '../screens/Details';
 import UnderConstruction from '../screens/UnderConstruction';
-
+import SignIn from '../screens/SignIn';
 const StackNavigation = createStackNavigator();
 
 export default function Stack() {
@@ -12,6 +12,7 @@ export default function Stack() {
             <StackNavigation.Screen name="Home" component={Home} options={{headerShown: false}} />
             <StackNavigation.Screen name="Cities" component={Cities} options={{headerShown: false}} />
             <StackNavigation.Screen name="Details" component={Details} options={{headerShown: false}} />
+            <StackNavigation.Screen name="SignIn" component={SignIn} options={{headerShown: false}} />
         </StackNavigation.Navigator>
     );
 }
